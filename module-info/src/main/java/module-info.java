58569ca89963d30f90e745f62ebaf8f73065a3e7
Yusuke Yamamoto <yusuke@mac.com>
2022-09-23 11:43:13 +0900

    Multi-Release JAR structure

--- module-info/src/main/java/module-info.java
+++ module-info/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module one.cafebabe.webdriverinstaller {
     exports one.cafebabe.webdriverinstaller;
     requires java.logging;
     requires java.xml;
-    requires org.slf4j;
+    requires static org.slf4j;
     requires org.apache.commons.compress;
 }
